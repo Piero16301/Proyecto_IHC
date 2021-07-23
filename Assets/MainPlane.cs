e9e9947a3e3abadc12e198f3cb5e5670a11abbd7
Piero Morales <piero.morales@utec.edu.pe>
2021-07-23 18:14:59 -0500

    Se agrega el cambio de vista y se inicia el soporte de mando

--- Assets/MainPlane.cs
+++ Assets/MainPlane.cs
@@ -5,34 +5,51 @@ using UnityEngine;
 public class MainPlane : MonoBehaviour
 {
 	public float speed = 0.0f;
+	public Camera FirstPersonCam;
+	public bool cameraSwitch = false;
+	public Canvas FirstPersonCanvas;
+	Camera ThirdPersonCam;
 	
 	// Start is called before the first frame update
 	void Start()
 	{
 		Debug.Log("plane pilot script added to: " + gameObject.name);
+		ThirdPersonCam = Camera.main;
+		FirstPersonCanvas.gameObject.SetActive(false);
+		ThirdPersonCam.enabled = true;
+		FirstPersonCam.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
+		if (Input.GetKeyDown(KeyCode.V)) {
+			if (FirstPersonCam.enabled) {
+				ThirdPersonCam.enabled = true;
+				FirstPersonCam.enabled = false;
+				FirstPersonCanvas.gameObject.SetActive(false);
+			}
+			else if (!FirstPersonCam.enabled) {
+				ThirdPersonCam.enabled = false;
+				FirstPersonCam.enabled = true;
+				FirstPersonCanvas.gameObject.SetActive(true);
+			}
+		}
+
 		Vector3 moveCamTo = transform.position - transform.forward * 80.0f + Vector3.up * 30.0f;
 		float bias = 0.96f;
-		Camera.main.transform.position = Camera.main.transform.position * bias +
+		ThirdPersonCam.transform.position = ThirdPersonCam.transform.position * bias +
 			moveCamTo * (1.0f - bias);
-		Camera.main.transform.LookAt(transform.position + transform.forward * 50.0f);
+		ThirdPersonCam.transform.LookAt(transform.position + transform.forward * 50.0f);
 		
 		transform.position += transform.forward * Time.deltaTime * speed;
 		
 		speed -= transform.forward.y * Time.deltaTime * 50.0f;
-		
-		if (Input.GetKey(KeyCode.W)) {
-			speed += Time.deltaTime * 20.0f;
+
+		if (Input.GetAxis ("Speed") != 0.0f) {
+			speed += Input.GetAxis ("Speed") * Time.deltaTime * 20.0f;
 		}
-		
-		if (Input.GetKey(KeyCode.S)) {
-			speed -= Time.deltaTime * 20.0f;
-		}
-		
+
 		/*if (speed < 0.0f) {
 			speed = 0.0f;
 		}*/
