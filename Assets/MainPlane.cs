2e3e44d01328acf1a327accb3d87375c95ead8ad
Piero Morales <piero.morales@utec.edu.pe>
2021-07-27 19:09:48 -0500

    Termino de la interfaz del mundo

--- Assets/MainPlane.cs
+++ Assets/MainPlane.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
 
 public class MainPlane : MonoBehaviour
 {
@@ -9,27 +14,36 @@ public class MainPlane : MonoBehaviour
 	public bool cameraSwitch = false;
 	public Canvas FirstPersonCanvas;
 	Camera ThirdPersonCam;
-	
+
+	// URL para el POST y GET
+	//readonly string getURL = "http://ec2-3-17-189-111.us-east-2.compute.amazonaws.com/GETSimulador.php";
+	readonly string postURL = "http://ec2-3-17-189-111.us-east-2.compute.amazonaws.com/POSTSimulador.php";
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		Debug.Log("plane pilot script added to: " + gameObject.name);
+		UnityEngine.Debug.Log("Script agregado a objeto: " + gameObject.name);
 		ThirdPersonCam = Camera.main;
 		FirstPersonCanvas.gameObject.SetActive(false);
 		ThirdPersonCam.enabled = true;
 		FirstPersonCam.enabled = false;
 	}
-	
+
+	float elapsed = 0f;
+
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.V)) {
-			if (FirstPersonCam.enabled) {
+		if (Input.GetKeyDown(KeyCode.V) || Input.GetKeyDown(KeyCode.JoystickButton0)) //Square key on DS4
+		{
+			if (FirstPersonCam.enabled)
+			{
 				ThirdPersonCam.enabled = true;
 				FirstPersonCam.enabled = false;
 				FirstPersonCanvas.gameObject.SetActive(false);
 			}
-			else if (!FirstPersonCam.enabled) {
+			else if (!FirstPersonCam.enabled)
+			{
 				ThirdPersonCam.enabled = false;
 				FirstPersonCam.enabled = true;
 				FirstPersonCanvas.gameObject.SetActive(true);
@@ -41,30 +55,69 @@ public class MainPlane : MonoBehaviour
 		ThirdPersonCam.transform.position = ThirdPersonCam.transform.position * bias +
 			moveCamTo * (1.0f - bias);
 		ThirdPersonCam.transform.LookAt(transform.position + transform.forward * 50.0f);
-		
+
 		transform.position += transform.forward * Time.deltaTime * speed;
-		
+
 		speed -= transform.forward.y * Time.deltaTime * 50.0f;
 
-		if (Input.GetAxis ("Speed") != 0.0f) {
-			speed += Input.GetAxis ("Speed") * Time.deltaTime * 20.0f;
+		if (Input.GetAxis("Speed") != 0.0f)
+		{
+			speed += Input.GetAxis("Speed") * Time.deltaTime * 20.0f;
 		}
 
-		/*if (speed < 0.0f) {
+		if (speed < 0.0f) {
 			speed = 0.0f;
-		}*/
-		
-		if (speed > 100.0f) {
-			speed = 100.0f;
 		}
-		
-		transform.Rotate(0.15f * Input.GetAxis("Vertical"), 0.1f * Input.GetAxis("Direction"), 0.2f * -Input.GetAxis("Horizontal"));
-		
+
+		if (speed > 200.0f)
+		{
+			speed = 200.0f;
+		}
+
+		transform.Rotate(0.2f * Input.GetAxis("Vertical"), 0.2f * Input.GetAxis("Direction"), 0.2f * -Input.GetAxis("Horizontal"));
+
+		//Envío mensaje servidor
+		elapsed += Time.deltaTime;
+		if (elapsed >= 1f)
+		{
+			// Se hace el parseo del JSON con los angulos en este momento de tiempo
+			//string json = "{\"x\":\"" + Math.Round(transform.rotation.eulerAngles.x, 2) + "\",\"y\":\"" + Math.Round(transform.rotation.eulerAngles.y, 2) + "\",\"z\":\"" + Math.Round(transform.rotation.eulerAngles.z, 2) + "\"}";
+
+			double xValue = Math.Round(transform.rotation.eulerAngles.x, 2);
+			double yValue = Math.Round(transform.rotation.eulerAngles.y, 2);
+			double zValue = Math.Round(transform.rotation.eulerAngles.z, 2);
+
+			string xAngle = "G" + xValue.ToString("G", CultureInfo.InvariantCulture);
+			string yAngle = "G" + yValue.ToString("G", CultureInfo.InvariantCulture);
+			string zAngle = "G" + zValue.ToString("G", CultureInfo.InvariantCulture);
+
+			// StartCoroutine(SimplePostRequest(xAngle, yAngle, zAngle));
+
+			UnityEngine.Debug.Log("x:" + xAngle + " y:" + yAngle + " z:" + zAngle);
+			elapsed %= 1f;
+		}
+
 		float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
-		if (terrainHeightWhereWeAre > transform.position.y) {
-			transform.position = new Vector3(transform.position.x,
-			                                 terrainHeightWhereWeAre + 2,
-			                                 transform.position.z);
+		if (terrainHeightWhereWeAre > transform.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre + 2, transform.position.z);
+		}
+	}
+
+	IEnumerator SimplePostRequest(string xAngle, string yAngle, string zAngle)
+	{
+		List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
+		wwwForm.Add(new MultipartFormDataSection("xValue", xAngle));
+		wwwForm.Add(new MultipartFormDataSection("yValue", yAngle));
+		wwwForm.Add(new MultipartFormDataSection("zValue", zAngle));
+
+		UnityWebRequest www = UnityWebRequest.Post(postURL, wwwForm);
+
+		yield return www.SendWebRequest();
+
+		if (www.isNetworkError || www.isHttpError)
+		{
+			UnityEngine.Debug.Log(www.error);
 		}
 	}
 }
